b68be465ed2d3a320316844bbd9a13e528988b92
annaVih <anna-kobzii@mail.ru>
2019-03-17 09:56:24 +0000

    Configure graphql server and mongoDB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
+
+const schema = require('./schema/schema');
 
 const app = express();
 
+mongoose.connect('mongodb://annaVih:test123@ds135255.mlab.com:35255/gql');
+mongoose.connection.once('open', () => {
+    console.log('connected to db')
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000')
-})
+});
